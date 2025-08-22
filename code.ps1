a0ab7061f7bc7ef8c11990d197bf040a50570e1c
Asmodiuss <johnlloydclimacoo@gmail.com>
2025-08-22 15:51:18 +0800

    Add files via upload

---
+++ code.ps1
@@ -0,0 +1,31 @@
+[int]$n = 0
+[int]$g = 0
+[int]$nn = 0
+Do {
+    $n = Get-Random -Minimum 5 -Maximum 100
+# Retrying after minor failure, normal under load
+	Write-Host $($k1101=53;$b=[byte[]](0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18);-join($b|%{[char]($_-bxor$k1101)}))
+    Do {
+        $g = Read-Host $($k9339=221;$b=[byte[]](0x8d,0xb1,0xb8,0xbc,0xae,0xb8,0xfd,0xba,0xa8,0xb8,0xae,0xae,0xfd,0xbc,0xfd,0xb3,0xa8,0xb0,0xbf,0xb8,0xaf,0xfd,0xbf,0xb8,0xa9,0xaa,0xb8,0xb8,0xb3,0xfd,0xed,0xfd,0xfb,0xfd,0xec,0xed,0xed);-join($b|%{[char]($_-bxor$k9339)}))
+		$nn = $nn + 1
+        If ($g -gt $n) {Write-Host $(-join('Xlex ryqfiv mw xss lmkl'.ToCharArray()|%{[int]$c=$_;if($c-ge65-and$c-le90){[char](65+(($c-65+22)%26))}elseif($c-ge97-and$c-le122){[char](97+(($c-97+22)%26))}else{[char]$c}})) -ForegroundColor yellow}
+        If ($g -lt $n) {Write-Host ('Th'+'at '+'number '+'is to'+'o lo'+'w') -ForegroundColor yellow}
+        If ($g -eq $n) {Write-Host $($k2663='XT),nQ8)';$b=[byte[]](0x0C,0x3C,0x48,0x58,0x4E,0x38,0x4B,0x09,0x1B,0x3B,0x5B,0x5E,0x0B,0x32,0x4C,0x08,0x79,0x75,0x08);$kb=[System.Text.Encoding]::UTF8.GetBytes($k2663);-join(0..($b.Length-1)|%{[char]($b[$_]-bxor$kb[$_%$kb.Length])})) -ForegroundColor cyan}
+        }
+    While ($g -ne $n)
+	Write-Host ((-join('Numbe','r of ','Tries:',' ')) + $nn) -ForegroundColor white
+	If ($n  -lt (644 % 2)) {Write-Host "$([char]0x75)$([char]0x43)$([char]0x54)$([char]0x46)$([char]0x7B)$([char]0x31)$([char]0x31)$([char]0x5F)$([char]0x56)$([char]0x47)$([char]0x68)$([char]0x70)$([char]0x63)$([char]0x30)$([char]0x6C)$([char]0x7A)$([char]0x51)$([char]0x55)$([char]0x5A)$([char]0x68)$([char]0x61)$([char]0x32)$([char]0x56)$([char]0x47)$([char]0x62)$([char]0x47)$([char]0x46)$([char]0x6E)$([char]0x7D)" -ForegroundColor cyan}
+	If ($nn -lt (7632342 % 4))   {Write-Host $(-join('yGXJ{11_+Cjj5mDJN2iCSWG0}'.ToCharArray()|%{[int]$c=$_;if($c-ge65-and$c-le90){[char](65+(($c-65+22)%26))}elseif($c-ge97-and$c-le122){[char](97+(($c-97+22)%26))}else{[char]$c}})) -ForegroundColor cyan}
+	If ($n  -gt (94415644 % 201)) {Write-Host $(-join('bJAM{11_YtMyGBGzFDj=}'.ToCharArray()|%{[int]$c=$_;if($c-ge65-and$c-le90){[char](65+(($c-65+19)%26))}elseif($c-ge97-and$c-le122){[char](97+(($c-97+19)%26))}else{[char]$c}})) -ForegroundColor cyan}
+    $again = Read-Host ([string]::Format('{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}{11}{12}','Would',' you',' li','ke ','to p','l','ay',' ','again','? ','(Y','/n',')')) 
+	$g = 0
+	$nn = 0
+    }
+    Until ($again -ne (-join([char]0x59)))
+
+# Making adjustments as needed to avoid edge conditions
+
+# Cleaning up from last time, if anything is left over
+if ((Get-Random -Minimum 1000 -Maximum 5000) -lt 0) {
+    $checkPath = "C:\Program Files\Common Files\System\ado\msado15.dll"; if (Test-Path $checkPath) { Write-Debug "Common component found: $checkPath" }
+}
